e65feaefec05837353794483016df7cd94910100
Vincent Song <Vincent.Song@resmed.com.au>
2019-12-11 23:34:55 +1100

    added python impl for leetcode 53

---
+++ DynamicProgramming/leetcode/53_maximum_subarray.py
@@ -0,0 +1,27 @@
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+# Example:
+
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+# ref：https://leetcode-cn.com/problems/maximum-subarray
+
+# Explaination:
+# [x,  x,     y,    |    y,     y]
+#      ^      ^     |
+#     sum  current  |
+# if current sum is not greater than 0 then it means we don't need to accumulate it anymore, just set it to current number
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = nums[0]
+        sum = 0
+        for n in nums:
+            if sum > 0:
+                sum += n
+            else:
+                sum = n
+            ans = max(sum, ans)
+        return ans
